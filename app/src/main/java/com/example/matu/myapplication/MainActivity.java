2bd3474da9eafdfa576a1a4382695cc35b5dbc6d
matias <matti1020009@gmail.com>
2018-04-15 17:31:32 -0300

    include zxing to proyect

---
+++ app/src/main/java/com/example/matu/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.matu.myapplication;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView scanerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void escanerQR(View view) {
+        scanerView = new ZXingScannerView(this);
+        setContentView(scanerView);
+        scanerView.setResultHandler(this);
+        scanerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Resultado del escaner");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
